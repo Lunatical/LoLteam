607cd8f337dbff5478ba071369fe4ce4f7a8415d
Lunatical <gpdlekdns@gmail.com>
2013-09-13 16:48:28 +0900

    User Team edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,13 +20,6 @@ class UsersController < ApplicationController
     @team = Team.all
   end
   
-  def show
-    @user = User.find(params[:id])
-    @team = @user.team
-    @champions = @user.champions
-    @matches = Match.all
-  end
-
   def create
     @user = User.new(user_params)
     if @user.save
@@ -35,10 +28,27 @@ class UsersController < ApplicationController
       render :new
     end
   end
+    
+  def show
+    @user = User.find(params[:id])
+    @team = @user.team
+    @teams = Team.all
+    @champions = @user.champions
+    @matches = Match.all
+  end
+  
+  def update
+    current_user.update_attribute(:team_id, params[:user][:team_id])
+    redirect_to :back 
+  end
+
   
   private 
   
   def user_params
     params.require(:user).permit(:name, :game_nickname, :top, :ap, :ad, :mom, :jungle, :team_id, :email, :password)
+  end
+  def user_team_params
+    params.require(:user).permit(:team_id)
   end
 end
